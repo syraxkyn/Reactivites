acea18b9ce614f728bca683fe19fc4d8db94f80e
andrey <ash03@tut.by>
2023-11-27 22:58:25 +0300

    section 64

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Activity } from '../../../app/models/activity';
+import { Button, Item, Label, Segment } from 'semantic-ui-react';
+
+interface Props {
+    activites: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean
+}
+
+export default function ActivityList({ activites, selectActivity, deleteActivity, submitting }: Props) {
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(e: any, id:string){
+
+    }
+
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activites.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>{activity.city}, {activity.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button
+                                    name={activity.id}
+                                    loading={submitting}
+                                    onClick={() => deleteActivity(activity.id)}
+                                    floated='right'
+                                    content='Delete'
+                                    color='red' />
+                                <Label basic content={activity.category} />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+}
