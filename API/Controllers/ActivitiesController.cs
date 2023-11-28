3baf31b97490153e2b1fe2395aa7603a44d84bde
andrey <ash03@tut.by>
2023-11-28 19:06:55 +0300

    End of section 7

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,26 +10,38 @@ namespace API.Controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await Mediator.Send(new Details.Query{Id=id});
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateActivity([FromBody]Activity activity){
-            await Mediator.Send(new Create.Command{Activity = activity});
+        public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
             return Ok();
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> EditActivity(Guid id,[FromBody]Activity activity){
+        public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
+        {
             activity.Id = id;
 
-            await Mediator.Send(new Edit.Command{Activity=activity});
+            await Mediator.Send(new Edit.Command { Activity = activity });
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id});
 
             return Ok();
         }
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import App from './app/layout/App'
+import { StoreContext, store } from './app/stores/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
 )
